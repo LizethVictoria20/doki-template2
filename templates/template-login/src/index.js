ae7f10d2c2162176f55e6c5c4ea5e77bdeb434a9
Liz Victoria Franco <lizvictoria754@gmail.com>
2021-02-15 13:40:03 -0500

    Adding new template

---
+++ templates/template-login/src/index.js
@@ -0,0 +1,11 @@
+/* Template to login */
+
+const container = document.querySelector('#app')
+container.className = 'h-screen  bg-no-repeat'
+
+const containerLogin = document.createElement('div')
+containerLogin.innerText = 'holis'
+containerLogin.className = 'containerLogin bg-white w-2/4 mt-48'
+
+
+container.appendChild(containerLogin)
